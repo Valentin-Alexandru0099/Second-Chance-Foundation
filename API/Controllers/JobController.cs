33d3f26e4608b0ef8c9f587d452ed1c670f038e6
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-04-15 14:18:53 -0700

    add JobsController

--- API/Controllers/JobController.cs
+++ API/Controllers/JobController.cs
@@ -1,6 +1,29 @@
-﻿namespace API.Controllers
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
 {
-    public class JobController
+    public class JobController : BaseApiController
     {
+        private readonly StoreContext _context;
+        public JobController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Job>>> GetAllJobs()
+        {
+            return await _context.Jobs.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetJob(int id)
+        {
+            return await _context.Jobs.FindAsync(id);
+        }
+
     }
 }
