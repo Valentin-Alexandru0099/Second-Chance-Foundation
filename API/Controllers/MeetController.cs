cc333be84b92e0a6c3a7e6660cc3db41eeb05731
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-04-15 14:23:17 -0700

    add MeetController

---
+++ API/Controllers/MeetController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class MeetController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public MeetController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Meet>>> GetAllMeets()
+        {
+            return await _context.Meets.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Meet>> GetJob(int id)
+        {
+            return await _context.Meets.FindAsync(id);
+        }
+    }
+}
