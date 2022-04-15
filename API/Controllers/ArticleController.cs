577f1231d0e777dbf4389b6cb476f101b3c46019
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-04-15 14:25:50 -0700

    add ArticleController

---
+++ API/Controllers/ArticleController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ArticleController
+    {
+        private readonly StoreContext _context;
+        public ArticleController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Article>>> GetAllArticles()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Article>> GetArticle(int id)
+        {
+            return await _context.Articles.FindAsync(id);
+        }
+    }
+}
