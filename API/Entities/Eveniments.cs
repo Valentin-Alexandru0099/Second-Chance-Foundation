1b6a6f3182970429beeec83331cdaf4b47b3a3e0
Valentin-Alexandru0099 <dumitru.valentin.alexandru0@gmail.com>
2022-04-15 10:16:47 -0700

    add initial entities for database

---
+++ API/Entities/Eveniments.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Entities
+{
+    public class Eveniments
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime SubbmissionTime { get; set; }
+        public Status Status { get; set; }
+
+        public int OrganizationId { get; set; }
+        
+
+    }
+}
