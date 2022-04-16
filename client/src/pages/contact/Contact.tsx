4f87000a103a5a8924d77cea869e815b1fdcb2f8
annarad111 <radulescuana111@gmail.com>
2022-04-16 05:46:57 +0300

    Styling homepage

--- client/src/pages/contact/Contact.tsx
+++ client/src/pages/contact/Contact.tsx
@@ -1,4 +1,5 @@
 import { Typography } from "@mui/material";
+import LocalPhoneTwoToneIcon from '@mui/icons-material/LocalPhoneTwoTone';
 
 import GoogleMap from './Map/GoogleMap';
 import './Contact.css';
@@ -18,6 +19,10 @@ export default function Contact() {
             <Typography variant='h3' className="contacttitle">Contact</Typography>
             <div className="mapdiv">
                 <GoogleMap location={location} zoomLevel={19}></GoogleMap>
+                </div>
+                <div className="centerdiv">
+                    <LocalPhoneTwoToneIcon/>
+                    <p className="phonetext">Phone: +4024414670 </p>
                 </div>
         </>
     )
